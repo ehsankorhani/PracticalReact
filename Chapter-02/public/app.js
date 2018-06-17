d36efd7e16398b41d551a3b556b5938b5c6e6c2f
Ehsan Korhani <Ehsan.Korhani@veda.com.au>
2018-06-17 13:45:48 +1000

    Chapter 2

---
+++ Chapter-02/public/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var template = React.createElement(
+  "p",
+  null,
+  "Hello React with JSX!"
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
---
+++ Chapter-02/src/app.js
@@ -0,0 +1,5 @@
+var template = <p>Hello React with JSX!</p>;
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
